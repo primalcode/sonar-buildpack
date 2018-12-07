81c31dcc1c7b583eb4166bf0e4e6cecadafe46e1
Yoshima FUKUDA <yoshima.fukuda@codeberry.co.jp>
2018-12-07 20:55:54 +0900

    edit sonar version

--- vars.sh
+++ vars.sh
@@ -3,4 +3,4 @@
 export JRE_URL=https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jre-8u191-linux-x64.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
-export SONARQUBE_ZIP="sonarqube-7.4.zip"
+export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
