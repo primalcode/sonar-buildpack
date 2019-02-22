ec9ed7da51b35ab4d4018df12a31244f3b5b66ea
primalcode <yoshima.fukuda@gmail.com>
2019-02-22 12:55:09 +0900

    Update startSonarQube.sh

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -7,7 +7,10 @@ echo "-----> Setting sonar.properties"
 echo "       sonar.web.port=${PORT}"
 echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
 echo "sonar.web.port=${PORT}\n" >> ./sonar.properties
-
+echo "sonar.jdbc.username=${JDBC_NAME}\n" >> ./sonar.properties
+echo "sonar.jdbc.password=${JDBC_PASS}\n" >> ./sonar.properties
+echo "sonar.jdbc.url=${JDBC_URL}\n" >> ./sonar.properties
+cat ./sonar.properties
 # Replace all environment variables with syntax ${MY_ENV_VAR} with the value
 # thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
 perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties
