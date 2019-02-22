b0d8ec682dd7b81c91bcb3843eb4819dbe5df262
primalcode <yoshima.fukuda@gmail.com>
2019-02-22 10:46:07 +0900

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jre-8u191-linux-x64.tar.gz
+export JRE_URL=https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
